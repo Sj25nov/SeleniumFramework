a83bf723d961750ab579b2de6289eadb8d5c8167
Saurabh <saurabhj1414@gmail.com>
2026-02-09 19:51:28 +0530

    commited

--- src/test/java/ex_09022026_Waits/implicitwait.java
+++ src/test/java/ex_09022026_Waits/implicitwait.java
@@ -4,18 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Threadsleep {
-    public static void main(String[] args) throws InterruptedException {
+import java.time.Duration;
+
+public class implicitwait {
+    public static void main(String[] args) {
+
 
         WebDriver driver=new ChromeDriver();
 
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         driver.manage().window().maximize();
-
-      Thread.sleep(10000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+       // Thread.sleep(10000);
 
         driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Saurabh");
-
+        System.out.println();
     }
 }
