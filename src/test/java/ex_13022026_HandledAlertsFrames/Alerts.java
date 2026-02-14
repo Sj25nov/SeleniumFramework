7f1e16834f27d3842cb0fe3069d7e8d4c9d293a7
Saurabh <saurabhj1414@gmail.com>
2026-02-14 12:13:26 +0530

    commited

--- src/test/java/ex_13022026_HandledAlertsFrames/Alerts.java
+++ src/test/java/ex_13022026_HandledAlertsFrames/Alerts.java
@@ -1,4 +1,26 @@
 package ex_13022026_HandledAlertsFrames;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Alerts {
+
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//open normal alert box
+        driver.findElement(By.xpath("//button[normalize-space()='Click for JS Alert']")).click();
+driver.switchTo().alert();
+        System.out.println(driver.getTitle());
+        //Normal Alert with OK Button
+
+
+    }
 }
