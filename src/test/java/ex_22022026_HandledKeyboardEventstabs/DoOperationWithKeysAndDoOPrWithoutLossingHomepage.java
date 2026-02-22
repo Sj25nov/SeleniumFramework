9a330c19037a301dccc9723e3068e6df4493c0b2
Saurabh <saurabhj1414@gmail.com>
2026-02-22 16:28:10 +0530

    commited

--- src/test/java/ex_22022026_HandledKeyboardEventstabs/DoOperationWithKeysAndDoOPrWithoutLossingHomepage.java
+++ src/test/java/ex_22022026_HandledKeyboardEventstabs/DoOperationWithKeysAndDoOPrWithoutLossingHomepage.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DoOperationWithKeysAndDoOPrWithoutLossingHomepage {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -29,11 +29,20 @@ List<String> TheElements=new ArrayList<> (driver.getWindowHandles());
 driver.switchTo().window(TheElements.get(1));
     String ExpectedURL=driver.getCurrentUrl();
 
+    driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("Saurabh Jadhav");
+Thread.sleep(3000);
+    driver.switchTo().window(TheElements.get(0));
+        Thread.sleep(3000);
+    WebElement serach=driver.findElement(By.xpath("(//input[@id='small-searchterms'])[1]"));
+    serach.sendKeys("T Shirts");
+        Thread.sleep(3000);
+    serach.click();
+
     if(ExpectedURL.equals("https://demo.nopcommerce.com/register?returnUrl=%2F")){
         System.out.println("Test Case Passed");
     }else {
         System.out.println("Test Case Failed");
     }
-
+        driver.quit();
     }
 }
