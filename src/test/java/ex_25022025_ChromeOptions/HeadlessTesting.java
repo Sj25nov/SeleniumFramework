0584f542919427795af054cdd0e3b6d3527faca6
Saurabh <saurabhj1414@gmail.com>
2026-02-25 11:55:47 +0530

    commited

---
+++ src/test/java/ex_25022025_ChromeOptions/HeadlessTesting.java
@@ -0,0 +1,31 @@
+package ex_25022025_ChromeOptions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class HeadlessTesting {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--headless=new");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demo.nopcommerce.com/");
+
+      //  driver.manage().window().maximize();
+        Thread.sleep(1000);
+        System.out.println(driver.getTitle());
+
+        String ActualTittle = driver.getTitle();
+
+        if (ActualTittle.contains("Just a moment")) {
+            System.out.println("Testcase Passed...!");
+        } else {
+            System.out.println("Testcase Failed...!");
+        }
+    }
+}
