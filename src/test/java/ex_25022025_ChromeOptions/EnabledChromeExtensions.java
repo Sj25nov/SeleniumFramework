d06766c8b95b8b053430f720664e3cd9e8545295
Saurabh <saurabhj1414@gmail.com>
2026-02-25 23:52:15 +0530

    commited

---
+++ src/test/java/ex_25022025_ChromeOptions/EnabledChromeExtensions.java
@@ -0,0 +1,21 @@
+package ex_25022025_ChromeOptions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+
+public class EnabledChromeExtensions {
+    public static void main(String[] args) throws InterruptedException {
+       // ChromeOptions options = new ChromeOptions();
+        ChromeOptions options=  new ChromeOptions();
+
+     File file=new File("G:\\AutomationCourseUdemyNotes\\AdBlock.crx");
+        options.addExtensions(file);
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://text-compare.com/");
+
+    }
+}
