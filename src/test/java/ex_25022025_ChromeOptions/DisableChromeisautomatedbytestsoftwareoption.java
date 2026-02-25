3766c90947c9c16f92598b198a43e8c3b85b117c
Saurabh <saurabhj1414@gmail.com>
2026-02-25 19:26:23 +0530

    commited

---
+++ src/test/java/ex_25022025_ChromeOptions/DisableChromeisautomatedbytestsoftwareoption.java
@@ -0,0 +1,8 @@
+package ex_25022025_ChromeOptions;
+
+public class DisableChromeisautomatedbytestsoftwareoption {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/test/java/ex_25022025_ChromeOptions/HandlingSSL.java
@@ -0,0 +1,26 @@
+package ex_25022025_ChromeOptions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class HandlingSSL {
+    public static void main(String[] args) {
+
+        ChromeOptions options=new ChromeOptions();
+options.setAcceptInsecureCerts(true);
+        WebDriver driver=new ChromeDriver(options);
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://expired.badssl.com/");
+
+
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+
+
+    }
+}
