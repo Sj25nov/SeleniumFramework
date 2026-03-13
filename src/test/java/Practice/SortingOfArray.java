4ca6bd43f546b7df71000e21a4578f971b860274
Saurabh <saurabhj1414@gmail.com>
2026-03-13 20:18:48 +0530

    commit

---
+++ src/test/java/Practice/SortingOfArray.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import java.util.Arrays;
+
+public class SortingOfArray {
+    public static void main(String[] args) {
+
+        int a[]={10,9,12,65,93,32};
+
+        System.out.println("Original Array is----->"+ Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println("Original Array is----->"+ Arrays.toString(a));
+    }
+}
