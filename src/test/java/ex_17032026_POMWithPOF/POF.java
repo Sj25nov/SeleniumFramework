6c6efe10db3fcdbca993fbda816c3e2135dca112
Saurabh <saurabhj1414@gmail.com>
2026-03-17 23:27:20 +0530

    commit

--- src/test/java/ex_17032026_POMWithPOF/POF.java
+++ src/test/java/ex_17032026_POMWithPOF/POF.java
@@ -1,4 +1,35 @@
 package ex_17032026_POMWithPOF;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class POF {
+    WebDriver driver;
+
+    POF(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//input[@placeholder='Username']")
+    WebElement GiveMeUserID;
+    @FindBy(xpath = "(//input[@placeholder='Password'])[1]")
+    WebElement GiveMePassword;
+    @FindBy(xpath = "//button[normalize-space()='Login']")
+    WebElement LoginButton;
+
+    public void Setusername(String user) {
+        GiveMeUserID.sendKeys(user);
+    }
+
+    public void SetPassword(String pass) {
+        GiveMePassword.sendKeys(pass);
+    }
+
+    public void clickLoginButton() {
+        LoginButton.click();
+
+    }
 }
--- src/test/java/ex_17032026_POMWithPOF/TestMethodForPOF.java
+++ src/test/java/ex_17032026_POMWithPOF/TestMethodForPOF.java
@@ -1,4 +1,37 @@
 package ex_17032026_POMWithPOF;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class TestMethodForPOF {
+    WebDriver driver;
+
+    @BeforeClass
+    void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+    }
+    @Test
+    void testLogin(){
+        POF PF=new POF(driver);
+        PF.Setusername("Admin");
+        PF.SetPassword("admin");
+        PF.clickLoginButton();
+
+        Assert.assertEquals(driver.getTitle(),"OrangeHRM");
+
+
+    }
+    @AfterClass
+    void teardown(){
+        driver.quit();
+    }
 }
